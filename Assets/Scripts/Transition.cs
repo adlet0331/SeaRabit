5f4f0f4529a641f44df1f409b79692eb8e8265ff
ddrr0077 <matt020601@gmail.com>
2025-01-12 04:08:52 +0900

    added clear portal

--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -9,9 +9,15 @@ public class Transition : MonoBehaviour
     [SerializeField] private Animator transitionAnimator;
     
     private readonly int _fadeOutHash = Animator.StringToHash("FadeOut");
+    private readonly int _fadeClearHash = Animator.StringToHash("FadeClear");
 
     public void FadeOut()
     {
         transitionAnimator.SetTrigger(_fadeOutHash);
+    }
+
+    public void FadeClear()
+    {
+        transitionAnimator.SetTrigger(_fadeClearHash);
     }
 }
